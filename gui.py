b54043e82c2520957e6559f89ceaa118a4942eba
Vignesh Pb <vignesh.pattalam@zoomrx.com>
2024-01-04 11:27:34 +0530

    Add Functionality to encode a URL and then display it in the UI and copy the encoded result to the clipboard automatically

--- gui.py
+++ gui.py
@@ -1,19 +1,36 @@
-from tkinter import *
+"""
+    GUI Application for commonly used Dev Operations
+"""
+from tkinter import Tk, Label, Entry, Button
+import urllib.parse
+import pyperclip
 
 root = Tk()
+root.title("DevToys")
 root.geometry("750x450")
 
-label = Label(root, text="Enter JSON Input")
-label.pack()
+url_label = Label(root, text="Enter URL that needs to be encoded")
+url_label.pack()
 
-entry = Entry(root, width=20)
-entry.focus_set()
-entry.pack()
+url_entry = Entry(root, width=80)
+url_entry.focus_set()
+url_entry.pack()
 
-def display_text():
-    print("Hello World")
+def encode_url_and_display_and_copy_to_clipboard():
+    """
+        Function to get the URL and encode it and then display it in the UI and
+        copy it to the clipboard
+    """
+    url_to_encode = url_entry.get()
+    encoded_url = urllib.parse.quote(url_to_encode, safe='')
+    encoded_url_label.config(text=encoded_url)
+    pyperclip.copy(encoded_url)
 
-button = Button(root, text="Validate JSON", command=display_text)
+
+button = Button(root, text="Encode URL and Copy 📝", command=encode_url_and_display_and_copy_to_clipboard)
 button.pack(pady=20)
+
+encoded_url_label = Label(root, text="Encoded URL will appear here")
+encoded_url_label.pack()
 
 root.mainloop()
